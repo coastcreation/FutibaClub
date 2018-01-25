51fab011067e775d8a85412b717faccffe7a9c9d
Diego Costa <diegocosta.91@outlook.com>
2018-01-25 17:17:00 -0200

    Commit Aula 2

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql2/promise')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 
 const account = require('./account')
+const admin = require('./admin')
+
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({
+    secret: 'fullstack-academy',
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.set('view engine','ejs')
 
@@ -18,7 +26,17 @@ const init = async() => {
         database: 'futibaclub'
     })
    
+    app.use((req, res, next) => {
+        if(req.session.user){
+            res.locals.user = req.session.user
+        }else{
+            res.locals.user = false
+        }
+        next()
+    })
+
     app.use(account(connection))
+    app.use('/admin', admin(connection))
 
     app.listen(3000, err => {
         console.log('Futiba Club server is running...')
